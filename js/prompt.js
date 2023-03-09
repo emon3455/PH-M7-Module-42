9061b482f5d18ea12160b66b749a81c56c8d4fee
emon3455 <emon.mhk69@gmail.com>
2023-03-09 23:26:18 +0600

    alert , confirm,prompt done

---
+++ js/prompt.js
@@ -0,0 +1,13 @@
+const ShowAlert=()=>{
+    alert("Alert Message");
+}
+
+const showConfirmation=()=>{
+    const message = confirm("Will you marry me?");
+    console.log(message);
+}
+
+const showPrompt=()=>{
+    const message = prompt("What is Your Name: ");
+    console.log(message);
+}
