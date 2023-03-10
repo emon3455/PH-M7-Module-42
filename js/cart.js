602286fb45d4502bfd8fcfeed33e87e681fd60ac
emon3455 <emon.mhk69@gmail.com>
2023-03-10 16:18:57 +0600

    :shopping cart done

--- js/cart.js
+++ js/cart.js
@@ -6,6 +6,9 @@ const addProduct=()=>{
 
     document.getElementById("product-name").value = "";
     document.getElementById("product-quantity").value ="";
+
+    saveToLocalStorage(product,quantity);
+
 }
 
 const displayProduct=(product, quantity)=>{
@@ -13,5 +16,31 @@ const displayProduct=(product, quantity)=>{
     const li = document.createElement("li");
     li.innerText=`${product}: ${quantity}`;
     container.appendChild(li);
-    
+
 }
+
+const getStoredShoppingCart=()=>{
+    let cart = {};
+    const storedCart = localStorage.getItem("cart");
+    if(storedCart){
+        cart = JSON.parse(storedCart);
+    }
+    return cart;
+}
+
+const saveToLocalStorage=(product , quantity)=>{
+    const cart = getStoredShoppingCart();
+    cart[product]=quantity; //this line helps us to add data in object
+    const cartStringify = JSON.stringify(cart);
+    localStorage.setItem("cart",cartStringify);
+}
+
+const displayProductFromLocalStorage=()=>{
+    const savedProduct = getStoredShoppingCart();
+    for (const product in savedProduct) {
+        const quantity = savedProduct[product];
+        displayProduct(product,quantity);
+    }
+}
+
+displayProductFromLocalStorage();
